1169e26a91dd296200ccad34724b0e2a15bc2000
sshridevi <sshridevi@gmail.com>
2020-03-16 10:40:44 -0700

    Hello in the morning added

--- seleniumClass/src/com/launch/Hello.java
+++ seleniumClass/src/com/launch/Hello.java
@@ -1,9 +1,7 @@
-package com.launch;
-
 public class Hello {
 
-	public static void main(String[] args) {
-		System.out.println("To see if it reflects in Github");
+	public static void main(String[] args)
+	{
+		System.out.println("Hello in the morning");
 	}
-
-}
+	}
