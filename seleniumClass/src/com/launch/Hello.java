6acd47504899349cc59ab0ad59e0a3f46abb1ece
sshridevi <sshridevi@gmail.com>
2020-03-16 10:42:38 -0700

    Hello in the afternoon added

--- seleniumClass/src/com/launch/Hello.java
+++ seleniumClass/src/com/launch/Hello.java
@@ -1,7 +1,10 @@
+package com.launch;
 public class Hello {
 
 	public static void main(String[] args)
 	{
 		System.out.println("Hello in the morning");
+		System.out.println("Hello in the afternoon");
+		///System.out.println("Hello in the evening");
 	}
 	}
